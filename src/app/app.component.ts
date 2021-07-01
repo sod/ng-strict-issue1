0c995c9efee8226659ae66dbe8417ceb62911e69
Alexander von Weiss <alexander.weiss@rebuy.com>
2021-07-01 15:20:26 +0200

    purge boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Feature {
+  lorem: ('a' | 'b' | 'c')[]
+  ipsum: ('a' | 'b' | 'c')[]
+}
+
 @Component({
   selector: 'app-root',
   template: `
+    {{ foo?.bar.includes('baz') }}
   `,
   styles: []
 })
 export class AppComponent {
+  features: Record<string, Feature> = {
+    a: {
+      lorem: ['a'],
+      ipsum: ['a'],
+    },
+    b: {
+      lorem: ['b'],
+      ipsum: ['b'],
+    }
+  }
+  feature: Feature | undefined = this.features.a;
 }
